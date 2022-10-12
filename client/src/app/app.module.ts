4b988dca560a9970de936a3f574ea3a01ad4de7a
OlehNoskov <olega6000@gmail.com>
2022-10-12 16:36:04 +0300

    create new module Angular project

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { CarComponent } from './car/car.component';
-import { CarsComponent } from './cars/cars.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CarComponent,
-    CarsComponent
+    AppComponent
   ],
   imports: [
     BrowserModule
